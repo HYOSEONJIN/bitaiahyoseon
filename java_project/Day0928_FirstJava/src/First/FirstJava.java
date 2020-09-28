afa4017fdd5baf30065d3cc78dbcd70de9dc6e5c
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-09-28 14:56:49 +0900

    Update FirstJava.java

--- java_project/Day0928_FirstJava/src/First/FirstJava.java
+++ java_project/Day0928_FirstJava/src/First/FirstJava.java
@@ -1,9 +1,13 @@
 package First;
 
+
+//First.firstJaca
 public class FirstJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Java");
+		System.out.println("안녕하세염!");
 
 	}
 
