07c7b9717c775bccf3bc61c34ef2173c39507322
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-09-28 16:48:25 +0900

    Update UseVariable.java
    20200928_JAVA

--- java_project/Day0928_FirstJava/src/First/UseVariable.java
+++ java_project/Day0928_FirstJava/src/First/UseVariable.java
@@ -26,10 +26,12 @@ public class UseVariable {
 		
 		// 5. 연산의 결과를 출력하자.
 		
-	//	System.out.println(num3);
+		System.out.println(num3);
 		
 		
 		
+		
+		// 실수 연산 오류
 		
 		
 		float n1 = 1.0000001f;
@@ -38,8 +40,33 @@ public class UseVariable {
 		
 		System.out.println((double)n1);
 		System.out.println(n1+n2);
+		
+		
+		// 캐릭터 값은 유니코드로 저장된다. 
+		 
+		char c1='A';
+		System.out.println(c1);
+		System.out.println((int)c1); //형변환
 
 		
+		
+		
+		// boolean : true/false
+		
+		boolean check = true;
+		
+		if(check) {   //논리값을 가지는 변수에 참조
+			System.out.println(true);
+			}
+		
+		int i =0;
+		
+		while(check) { // 반복의조건
+			if(1<5) {
+				break;
+			}
+			i++; // 탈출의 조건, 연산식
+		}
 	}
 
 }
