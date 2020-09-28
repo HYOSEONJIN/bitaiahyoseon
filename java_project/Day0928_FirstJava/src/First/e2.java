767c965d7dc99598ca256c4769f71cceb1a90451
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-09-28 18:42:23 +0900

    Update e2.java
    예외처리 연습2

--- java_project/Day0928_FirstJava/src/First/e2.java
+++ java_project/Day0928_FirstJava/src/First/e2.java
@@ -23,17 +23,19 @@ public class e2 {
 			b=false;
 			System.out.print("ID 입력 : ");
 			id=sc.next();
-			
 			small=id.toLowerCase(); // 대소문자 구별을 위해 소문자로 바꿔줌
+			
 			for(int i=0 ; i<small.length() ; i++) {
 				char a = small.charAt(i);
 				int asc = (int)a; // 아스키코드로 변경
-				if( a<97 || a>122 ) {
+				
+				if ( a<97 || a>122 ){
 					System.out.println("영문만 입력해주세요.");
 					b=true;
 					break;
 				}
 	}
+			
 			if(b==false) {
 				System.out.println("입력된 ID는 " + id);
 				break;
