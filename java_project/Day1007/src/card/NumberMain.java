50d188a47e2417ca0dd586ed533301a14576a152
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-07 18:11:33 +0900

    Create NumberMain.java

---
+++ java_project/Day1007/src/card/NumberMain.java
@@ -0,0 +1,12 @@
+package card;
+
+public class NumberMain {
+
+	public static void main(String[] args) {
+	
+		Number n = new Number(5); // 매개변수(int number)를 반드시 전달하도록 되어있다. 
+		
+		System.out.println(n.getNumber());
+	}
+
+}
