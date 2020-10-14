0dc03921e6225800bb4fb46721c5b311f0e06b73
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-14 09:49:52 +0900

    Create User.java

---
+++ java_project/Oct/src/Arr/User.java
@@ -0,0 +1,23 @@
+package Arr;
+
+public class User {
+
+		
+		// 사용자의 데이터를 저장 : 이름, 아이디(숫자)
+		// 김태형(1), 민윤기(2)
+		
+		int id;				// 사용자의 아이디
+		String name;		// 사용자의 이름
+
+		
+		// 생성자
+		User(int id, String name){
+			this.id=id;
+			this.name=name;
+		}
+		
+		void showData() {
+			System.out.println(name+"("+id+")");
+		}
+
+}
