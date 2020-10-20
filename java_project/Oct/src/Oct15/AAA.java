b20616af46b4b8fcc7a3bffa680072753d04c952
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-20 17:34:37 +0900

    Create AAA.java

---
+++ java_project/Oct/src/Oct15/AAA.java
@@ -0,0 +1,25 @@
+package Oct15;
+
+class AAA {
+
+	// public AAA(){}
+	
+	int num;
+	
+	public static int n=100;
+	
+	private AAA() {
+		
+	}
+	
+	// getter
+	public int getNum() {
+		return num;
+	}
+
+	// setter 
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+}
