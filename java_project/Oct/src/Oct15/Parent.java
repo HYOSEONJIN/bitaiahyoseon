1200eafd826a575914b9580196cad4ef09c22c84
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-20 17:34:52 +0900

    Create Parent.java

---
+++ java_project/Oct/src/Oct15/Parent.java
@@ -0,0 +1,17 @@
+package Oct15;
+
+public class Parent {
+
+	
+	int money=100;
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	
+}
