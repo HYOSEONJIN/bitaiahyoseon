1fbef9cc792f12fdc8ac59472bae70a770dd66b9
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-20 17:34:54 +0900

    Create SingletoneMain.java

---
+++ java_project/Oct/src/Oct15/SingletoneMain.java
@@ -0,0 +1,12 @@
+package Oct15;
+
+public class SingletoneMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Singleton s = new Singleton();
+		Singleton s= Singleton.getInstance();
+	}
+
+}
