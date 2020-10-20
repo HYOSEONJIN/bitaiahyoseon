6d56d95774d780fa442ea1251402579e78310f81
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-20 17:36:01 +0900

    Create PlayerMain.java

---
+++ java_project/Oct/src/Oct17/PlayerMain.java
@@ -0,0 +1,15 @@
+package Oct17;
+
+public class PlayerMain {
+
+	public static void main(String[] args) {
+
+	//	player player = new player();
+	//	VideoPlayer player = new VideoPlayer();
+		
+		AudioPlayer player = new AudioPlayer();
+		
+		//cannot instantiate the type 클래스명 > 추상클래스는 인스턴스를 생성할 수 없다.
+	}
+
+}
