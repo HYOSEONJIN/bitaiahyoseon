eb7b37b35bc0ad66d9c97228925cd1216c452659
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-20 17:36:18 +0900

    Create Fightable.java

---
+++ java_project/Oct/src/Oct17/Fightable.java
@@ -0,0 +1,17 @@
+package Oct17;
+
+public interface Fightable {
+
+	
+	//인터페이스에서는 퍼블릭어쩌구웅앵 생략가능
+	
+	//public abstract void move(); 
+	void move();
+	//public abstract void attack();
+	void attack();
+	
+	//public static final int NUM=10;
+	int NUM=10;
+	
+	
+}
