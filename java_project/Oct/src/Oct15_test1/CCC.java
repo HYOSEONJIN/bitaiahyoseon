54af7a28729d889e4f483adf2f96604ec72f845a
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-20 17:34:59 +0900

    Create CCC.java

---
+++ java_project/Oct/src/Oct15_test1/CCC.java
@@ -0,0 +1,18 @@
+package Oct15_test1;
+
+import Oct15.AAA;
+
+public class CCC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AAA.n=0;
+		
+		AAA a = new AAA();
+		
+		a.num=10;
+		
+	}
+
+}
