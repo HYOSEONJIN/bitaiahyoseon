01c5abe5dc7a4f5790179e693418e4646349507f
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-21 18:32:40 +0900

    Update companyInfor.java

--- java_project/FriendManager/src/Friend/companyInfor.java
+++ java_project/FriendManager/src/Friend/companyInfor.java
@@ -1,11 +1,11 @@
 package Friend;
 
-public class companyInfor extends Friend {
+public class CompanyInfor extends Friend {
 
 	private String company; // 회사 
 	private String email;
 	
-	companyInfor(String name, String pNum, String addr, String cpn, String em) {
+	CompanyInfor(String name, String pNum, String addr, String cpn, String em) {
 		super(name, pNum, addr);
 		this.company=cpn;
 		this.email=em;
@@ -19,11 +19,9 @@ public class companyInfor extends Friend {
 		System.out.println("이메일 : " + email);
 	}
 
-	@Override // 원래 비어있는 메소드였음
+	@Override
 	public void showBasicInfo() {
-		System.out.println("이름 : " + getName()); // private이므로 getName생성해서 불러움
-		System.out.println("전화번호 : " + getPhoneNumber());
-		System.out.println("주소 : " + getAddr());
+			super.showData();
 	}
 	
 }
