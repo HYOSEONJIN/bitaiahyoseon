f0119339deb2d2c204872ab2a489563ea09993da
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-21 18:32:50 +0900

    Update HighFriend.java

--- java_project/FriendManager/src/Friend/HighFriend.java
+++ java_project/FriendManager/src/Friend/HighFriend.java
@@ -2,28 +2,24 @@ package Friend;
 
 public class HighFriend extends Friend {
 
-	private String work ; // 직업을 추가해줌
-	
-	// 인스턴스 변수 초기화									// 워크추가
+	private String work; // 직업을 추가해줌
+
+	// 인스턴스 변수 초기화 // 워크추가
 	HighFriend(String name, String pNum, String addr, String work) {
 		super(name, pNum, addr);
-		this.work=work;
+		this.work = work;
 	}
 
 	@Override
-	public void showData() {   // Friend f = new HighFriend() > f.showDate();
+	public void showData() { 
 		super.showData();
 		System.out.println("직업 : " + work);
-		// 원래 showData메서드가 가진 기능(super)에 새로운 기능을 추가해줌!
+		
 	}
 
 	@Override
 	public void showBasicInfo() {
-		System.out.println("이름 : " + getName()); // private이므로 getName생성해서 불러움
-		System.out.println("전화번호 : " + getPhoneNumber());
-		System.out.println("주소 : " + getAddr());
+		super.showData();
 	}
-	
-
 
 }
