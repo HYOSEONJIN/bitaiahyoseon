8d19ce4a8087f5035527d2f400baa92e053c9b3a
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-21 18:32:54 +0900

    Delete Menu.java

--- java_project/FriendManager/src/Friend/Menu.java
+++
@@ -1,9 +0,0 @@
-package Friend;
-
-public interface Menu {
-	
-	
-	// public static final
-	int INSERT_HIGH=1, INSERT_UNIV=2, INTSERT_COM=3, INSERT_CLUB=4, INSERT_ALL=5, INSERT_BASIC=6, EXIT=7;
-
-}
