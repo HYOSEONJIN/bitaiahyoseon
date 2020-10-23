1889587999d801c94b6b000935c734c9e32c44e0
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-10-23 17:08:29 +0900

    Update TreeSetTest.java

--- java_project/Java/src/Collection/TreeSetTest.java
+++ java_project/Java/src/Collection/TreeSetTest.java
@@ -1,10 +1,36 @@
 package Collection;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class TreeSetTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		// Treeset : 트리 구조로 저장하는 자료구조. 2진트리
+
+		// Treeset 인스턴스 생성
+		TreeSet<Integer> tSet = new TreeSet<Integer>();
+		
+
+		tSet.add(1);
+		tSet.add(5);
+		tSet.add(2);
+		tSet.add(4);
+		tSet.add(3);
+		tSet.add(2);
+		
+		System.out.println("Treeset 요소의 갯수 : " + tSet.size());
+		
+		Iterator<Integer> itr = tSet.iterator();
+		
+		
+		while(itr.hasNext()){
+			System.out.println(itr.next());
+		}
 
 	}
+
 
 }
