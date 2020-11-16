1d096f83890e8fe059febc6cc7314fd4307ddc4f
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-11-16 18:13:08 +0900

    phonebook_SQL

--- SQL/phonebookTEST1.sql
+++ SQL/phonebookTEST1.sql
@@ -7,18 +7,19 @@ create table phoneInfo_basic (
         fr_regdate      DATE            default sysdate
 );
  
+drop table phoneinfo_basic;
 create table phoneInfo_univ (
         idx             number(6)       primary key,
         fr_u_major      varchar2(20)    default 'N',
-        fr_u_year       number(1)       check (5>=fr_u_year AND fr_u_year>=1),
-        fr_ref          number(7)       references phoneinfo_basic(idx)
+        fr_u_year       number(1)       check (fr_u_year BETWEEN 5 and 1),
+        fr_ref          number(7)       not null references phoneinfo_basic(idx)
 );
  
  
 create table phoneinfo_com(
         idx             number(6)       primary key,
         fr_c_company    varchar2(20)    default 'N',
-        fr_ref          number(6)       references phoneinfo_basic(idx)
+        fr_ref          number(6)       not null references phoneinfo_basic(idx)
 );
  
  
@@ -43,10 +44,10 @@ create table phonebook(
  
 ----------------------------------------------------------------
  -- 시퀀스 생성
-create Sequence seq_basic_idx MINVALUE 1 start with 1 INCREMENT BY 1;
-create Sequence seq_univ_idx MINVALUE 1 start with 1 INCREMENT BY 1;
-create Sequence seq_com_idx MINVALUE 1 start with 1 INCREMENT BY 1;
-create Sequence seq_main_idx MINVALUE 1 start with 1 INCREMENT BY 1;
+create Sequence seq_basic_idx MINVALUE 0 start with 1 INCREMENT BY 1;
+create Sequence seq_univ_idx MINVALUE 0 start with 1 INCREMENT BY 1;
+create Sequence seq_com_idx MINVALUE 0 start with 1 INCREMENT BY 1;
+create Sequence seq_main_idx MINVALUE 0 start with 1 INCREMENT BY 1;
 
 drop sequence seq_basic_idx;
 drop sequence seq_univ_idx;
@@ -57,7 +58,7 @@ select * from phoneinfo_basic;
 delete from phoneinfo_basic;
 
 insert into phoneinfo_basic values (seq_basic_idx.nextval, '효선', '01012345678', 'tomnmo@naver.com', '서울', sysdate);
-insert into phoneinfo_basic values (seq_basic_idx.nextval, '선효', '01098765432', 'ttt_111@naver.com', '경기', sysdate);
+insert into phoneinfo_basic values (seq_basic_idx.nextval, '하하호호', '01098765432', 'ttt_111@naver.com', '경기', sysdate);
 insert into phoneinfo_basic values (seq_basic_idx.nextval, '레쓰비', '01087873383', 'a_3@gmail.com', '대전', sysdate);
 
 commit;
@@ -82,21 +83,35 @@ insert into phonebook values (seq_main_idx.nextval , 'com');
 
 ------------------------------------------------------------------
 
---create or replace view view_com
+--drop table testpb1;
+--create table testpb1
 --as
-select fr_ref, fr_name, b.fr_phonenumber, b.fr_email, b.fr_address, b.fr_regdate, c.fr_c_company
-from phoneinfo_basic b, phoneinfo_com c
-where b.idx=c.fr_ref
-;
-
---create or replace view view_univ
+--select c.FR_REF as ref, fr_name, b.fr_phonenumber, b.fr_email, b.fr_address, b.fr_regdate, c.FR_C_COMPANY
+--from phoneinfo_basic b, phoneinfo_com c
+--where b.idx=c.fr_ref
+--;
+--
+--select *
+--from phoneinfo_basic b NA TURAL JOIN phoneinfo_com c
+--;
+--
+--drop table testpb2;
+--
+--create table testpb2
 --as
-select fr_ref, fr_name, b.fr_phonenumber, b.fr_email, b.fr_address, b.fr_regdate, u.fr_u_major,u.fr_u_year
-from phoneinfo_basic b,phoneinfo_univ u
-where b.idx=u.fr_ref
-order by b.idx
-;
-
+--select u.FR_REF as ref, fr_name, b.fr_phonenumber, b.fr_email, b.fr_address, b.fr_regdate, u.fr_u_major,u.fr_u_year
+--from phoneinfo_basic b,phoneinfo_univ u
+--where b.idx=u.fr_ref
+--order by b.idx
+--;
+--
+--select *
+--from phoneinfo_basic b NATURAL JOIN phoneinfo_univ u
+--;
+--
+--select *
+--from testpb1 NATURAL JOIN testpb2
+--;
 
 
 
