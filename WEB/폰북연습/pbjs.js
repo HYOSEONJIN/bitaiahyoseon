3ffd04aba6c125eee2b0e74811c2000a3598e1d1
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-12-01 20:56:45 +0900

    폰북만들기

--- WEB/폰북연습/pbjs.js
+++ WEB/폰북연습/pbjs.js
@@ -1,5 +1,19 @@
-/*전화번호를 저장하는 함수*/
 
+
+// 정보 저장할 함수
+var phoneinfo = [];
+
+// 집어넣기 위한 생성자 함수!
+function Pinfo(name, pnum, ftype){
+    this.Sname = name;
+    this.Snum = pnum;
+    this.Stype = ftype;
+}
+
+
+
+
+/*전화번호를 저장하는 함수*/
 function savesubmit() {
     /*alert('OK');*/
 
@@ -40,7 +54,59 @@ function savesubmit() {
     if (check) {
         return false;
     }
+    
+    var info = new Pinfo(iname, inum, itype);    
+    console.log(itype + ' : ' +  inum + ' : ', itype);
+    
+    // 배열에 저장
+    phoneinfo.push(info);    
+    console.log('phoneinfo', phoneinfo);
+    
+    document.querySelector('#phonebookinput').reset();    
+    alert('등록완료!');
+    
+    // 리스트 출력
+    addlist();
+    
     return false;
+}
+
+/*리스트 추가해주는 함수*/
+function addlist(){
+    
+    /*tbody가져오기 <tr><td> 들어있는 것*/
+    var listrow = document.getElementById('listrow'); 
+    
+    var tbody='';
+    
+    for (var i=0; i<phoneinfo.length; i++){
+        
+        tbody+='<tr>';
+        tbody+='<td>'+i+'</td>';
+        tbody+='<td>'+phoneinfo[i].Sname+'</td>';
+        tbody+='<td>'+phoneinfo[i].Snum+'</td>';
+        tbody+='<td>'+phoneinfo[i].Stype+'</td>';
+        tbody+='<td><a>수정</a> <a href="javascript:delinfo('+i+')">삭제</a></td>';
+        tbody+='</tr>';
+        
+    }
+    
+    listrow.innerHTML = tbody;
+    
+    
+}
 
 
+/*삭제기능*/
+function delinfo(index){    
+    
+    var delChk = confirm('삭제하시겠습니까?');
+    
+    if(delChk){
+        phoneinfo.splice(index,1);
+        
+        alert('삭제완료!');
+              
+        addlist();
+    }
 }
