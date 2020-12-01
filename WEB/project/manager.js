025f69596a1f354941d4e65297ee0520a86401e9
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-12-01 16:25:51 +0900

    dd

--- WEB/project/manager.js
+++ WEB/project/manager.js
@@ -8,8 +8,7 @@ window.onload = function () {
 }*/
 
 
-
-var members = [];
+var members = []; // object > 문자열 JSON
 
 // 생성자 함수 Member
 
@@ -19,13 +18,42 @@ function Member(id, pw, name) {
     this.username = name;
 }
 
-Member.prototype.makeHTML = function(){
-    console.log(this.userid+' : '+this.userpw+" : " +this.username)
-    
+Member.prototype.makeHtml = function(index) {
+
+    console.log(this.userid + ' : ' + this.userpw + ' : ' + this.username);
+
+    var memberHtml = '';
+    memberHtml += '<tr>';
+    memberHtml += ' <td>' + index + '</td>';
+    memberHtml += ' <td>' + this.userid + '</td>';
+    memberHtml += ' <td>' + this.userpw + '</td>';
+    memberHtml += ' <td>' + this.username + '</td>';
+    memberHtml += ' <td><a href="javascript:editMember(' + index + ')">수정</a> <a href="javascript:delmember(' + index + ')">삭제</a></td>';
+    memberHtml += '<tr>';
+
+    return memberHtml;
+
+
 }
 
-function regmember() {
+window.onload = function () {
 
+    // 저장하는 key > members
+    // 저장값이 없으면 null 반환
+    if (localStorage.getItem('members') == null) {
+        // 초기 배열을 저장 : 배열을 문자열로 변경해서 저장
+        localStorage.setItem('members', JSON.stringify(members));
+    } else {
+        members = JSON.parse(localStorage.getItem('members'));
+    setlist();
+
+}
+}
+
+
+
+
+function regmember() {
 
     // 사용자가 입력한 데이터를 확인
     // 유효성 검사
@@ -92,15 +120,198 @@ function regmember() {
     }
 
 
+    /*var member = {
+        memberid: userid.value,
+        pw: pw.value,
+        membername: username.value
+    };*/
+
     var member = new Member(userid.value, pw.value, username.value);
-    
-//    console.log('member', member);
-    member.makeHTML();
-    
+
+    //console.log('member', member);
+    member.makeHtml()
+
     // 배열에 회원 정보를 저장
     members.push(member);
-    
+
     console.log('members', members);
 
+    document.querySelector('#regform').reset();
+    alert('등록되었습니다');
+
+    // 저장
+    localStorage.setItem('members', JSON.stringify(members));
+
+    // 리스트 갱신
+    setlist();
+
+
+
     return false;
+}
+
+
+// 리스테이블의 정렬
+function setlist() {
+
+    // table tbody 캐스팅
+    var listrow = document.querySelector('#listrow');
+
+    var tbody = '<tr>';
+    tbody += '<th>순번(index)</th>';
+    tbody += '<th>아이디</th>';
+    tbody += '<th>비밀번호</th>';
+    tbody += '<th>이름</th>';
+    tbody += '<th>관리</th>';
+    tbody += '</tr>';
+
+    for (var i = 0; i < members.length; i++) {
+        tbody += members[i].makeHtml(i);
+    }
+
+    listrow.innerHTML = tbody;
+}
+
+
+// 배열의 데이터를 삭제
+function delmember(index) {
+
+    var delChk = confirm('삭제하시겠습니까?');
+
+    if (delChk) {
+
+        // 삭제 -> 배열에서 요소를 삭제
+        members.splice(index, 1);
+
+
+        // 저장
+        localStorage.setItem('members', JSON.stringify(members));
+
+
+        alert('삭제되었습니다.');
+
+        // 배열의 변경된 내용으로 리스트 출력
+        setlist();
+
+    }
+
+}
+
+function editMember(index) {
+    console.log(members[index]);
+
+    // 수정 화면 캐스팅
+    var editDiv = document.querySelector('div.edit_div');
+    editDiv.style.display = 'block';
+
+    // form 안에 있는 input 캐스팅
+    // id
+    var eid = document.querySelector('#editid');
+    // name
+    var ename = document.querySelector('#editname');
+    // pw
+    var epw = document.querySelector('#editpw');
+    // repw
+    var erepw = document.querySelector('#editrepw');
+    // index
+    var idx = document.querySelector('#index');
+
+
+    idx.value = index;
+    eid.value = members[index].userid;
+    ename.value = members[index].username;
+    epw.value = members[index].userpw;
+}
+
+function editMemberData() {
+
+    // index
+    var eidx = document.querySelector('#index').value;
+    // id
+    //    var eid = document.querySelector('#editid');
+    // pw
+    var epw = document.querySelector('#editpw');
+    // repw
+    var erepw = document.querySelector('#editrepw');
+    // name
+    var ename = document.querySelector('#editname');
+
+
+    // editpw > mag
+    var pw_msg = document.querySelector('#editpw+div.msg');
+    var repw_msg = document.querySelector('#editrepw+div.msg');
+    var ename_msg = document.querySelector('#editname+div.msg');
+
+
+    var check = false; // 문제 없다
+
+    // 공백문자를 검사 : trim()을 이용해서 공백 제거 후 문자열 비교
+    if (epw.value.trim().length < 1) {
+        pw_msg.style.display = 'block';
+        check = true;
+    }
+    if (erepw.value.trim().length < 1 || pw.value != repw.value) {
+        repw_msg.style.display = 'block';
+        check = true;
+    }
+    if (ename.value.trim().length < 1) {
+        ename_msg.style.display = 'block';
+        check = true;
+    }
+
+
+    /*userid.onfocus = function(){
+        document.querySelector('#userid+div.msg').style.display='none';
+    }*/
+
+    pw.addEventListener('focus', function () {
+        repw_msg.style.display = 'none';
+        epw.value = '';
+    });
+
+    repw.addEventListener('focus', function () {
+        repw_msg.style.display = 'none';
+        erepw.value = '';
+    });
+
+    username.addEventListener('focus', function () {
+        ename_msg.style.display = 'none';
+        ename.value = '';
+    });
+
+    console.log(check);
+
+    if (check) {
+        return false;
+    }
+
+
+    // members[index] -> pw, name 수정
+
+    members[eidx].username = ename.value.trim();
+    members[eidx].userpw = epw.value.trim();
+    
+    // 저장
+    localStorage.setItem('members', JSON.stringify(members));
+    
+
+    alert('수정되었습니다.');
+
+    // 회원 리스트 갱신
+    setlist();
+    
+    document.querySelector('#editform').reset();
+    document.querySelector('#div.edit_div').style.display = 'none';
+
+
+    return false;
+
+
+}
+
+function editMemberClose() {
+
+    var editDiv = document.querySelector('div.edit_div');
+    editDiv.style.display = 'none';
+
 }
