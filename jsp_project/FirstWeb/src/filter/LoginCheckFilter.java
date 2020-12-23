875b207a399d9a9add37358d916cacebd18fbb35
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2020-12-23 16:15:33 +0900

    1223

---
+++ jsp_project/FirstWeb/src/filter/LoginCheckFilter.java
@@ -0,0 +1,55 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheckFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request,
+						ServletResponse response,
+						FilterChain chain)
+			throws IOException, ServletException {
+		
+		System.out.println("로그인 체크필터 실행");
+		
+		// 회원이 로그인 되어있는지 확인하는 Filter를 생성하려고 한다.
+		// 서블릿리퀘스트를 http서블릿리퀘스트로
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession();
+		
+//		boolean loginCheck = false;
+		
+		if(session!=null && session.getAttribute("loginInfo")!=null) {
+			chain.doFilter(request, response); // 정상실행
+//			loginCheck =true;
+		} else {
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("../member/loginForm.jsp");
+			dispatcher.forward(request, response);
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- jsp_project/PPOSEONproject/WebContent/member/loginForm.jsp
+++ jsp_project/PPOSEONproject/WebContent/member/loginForm.jsp
@@ -1,6 +1,8 @@
 <%@page import="util.CookieBox"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -31,9 +33,9 @@
 
 	<div class="contents">
 		<h2 class="content_title">로그인 Form</h2>
-		<hr>
+		<hr>	
 		<div class="content">
-			<form action="login.jsp" method="get">
+			<form action="<c:url value="/member/login.jsp"/>" method="get">
 				<table>
 					<tr>
 						<th><label for="userid">아이디</label></th>
