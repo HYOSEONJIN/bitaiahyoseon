f9e846f16a126d17f372f65702414f297ebdfdde
HYOSEONJIN <hyoseonjin1@gmail.com>
2021-01-20 13:01:46 +0900

    0120

--- spring_project/FirstSpring/src/main/java/com/aia/firstspring/controller/RestApiController.java
+++ spring_project/FirstSpring/src/main/java/com/aia/firstspring/controller/RestApiController.java
@@ -7,53 +7,70 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aia.firstspring.member.domain.Member;
+import com.aia.firstspring.member.domain.MemberRegRequest;
 import com.aia.firstspring.member.service.MemberListService;
+import com.aia.firstspring.member.service.MemberRegService;
 import com.aia.firstspring.member.service.MemberRestService;
 
 @RestController
 @RequestMapping("/rest/ver1/members")
 public class RestApiController {
-	
+
 	@Autowired
 	private MemberRestService restService;
-	
-	
+
 	/*
-	 @GetMapping("/{idx}") // RequestMapping(method=GET)과 같음, /rest/ver01/members/36와 같은 경로로 들어옴
-	// @GetMapping
-	public String getRest(
-			@PathVariable("idx") int idx ) {
-		return "@RestController를 사용한 응답처리 : " + idx;
-	}
-	*/
+	 * @GetMapping("/{idx}") // RequestMapping(method=GET)과 같음,
+	 * /rest/ver01/members/36와 같은 경로로 들어옴 // @GetMapping public String getRest(
+	 * 
+	 * @PathVariable("idx") int idx ) { return "@RestController를 사용한 응답처리 : " + idx;
+	 * }
+	 */
 	@Autowired
 	private MemberListService listService;
-	
+
 	@GetMapping
-	public List<Member> getMemberList(){
+	public List<Member> getMemberList() {
 		return listService.getMemberList();
 	}
-	
+
 	@GetMapping("/map")
-	public Map<Integer, Member> getMemberListMap(){
-		
+	public Map<Integer, Member> getMemberListMap() {
+
 		Map<Integer, Member> memberMap = new HashMap<Integer, Member>();
-		for(Member member : listService.getMemberList()) {
+		for (Member member : listService.getMemberList()) {
 			memberMap.put(member.getIdx(), member);
 		}
 		return memberMap;
+
+	}
+
+	@GetMapping("/{idx}")
+	public Member getMemberInfo(@PathVariable("idx") int idx) {
+
+		return restService.getMember(idx);
+	}
+
+	@Autowired
+	private MemberRegService regService;
+
+	// @RequestMapping(method=RequestMethod.POST) 과 같다.
+	@PostMapping
+	public String insertMember(@RequestBody MemberRegRequest regRequest) {
+		
+
+		System.out.println(regRequest);
+		System.out.println(regRequest.getToMember());
+
+		return regService.insertMember(regRequest.getToMember())>0 ? "Y" : "N";
 		
 	}
-	 
-		@GetMapping("/{idx}")
-		public Member getMemberInfo(
-				@PathVariable("idx") int idx) {
-			
-			return restService.getMember(idx);
-			
-		}
+
 }
