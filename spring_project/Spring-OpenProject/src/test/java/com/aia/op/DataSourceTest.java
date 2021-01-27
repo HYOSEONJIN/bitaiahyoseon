d6da1b45fc99e2fa439a6416035b1351dcfb7374
HYOSEONJIN <71997946+HYOSEONJIN@users.noreply.github.com>
2021-01-27 15:48:54 +0900

    0127

--- spring_project/Spring-OpenProject/src/test/java/com/aia/op/DataSourceTest.java
+++ spring_project/Spring-OpenProject/src/test/java/com/aia/op/DataSourceTest.java
@@ -14,18 +14,20 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-//@Log4j
+@Log4j
 public class DataSourceTest {
 	
 	//@Autowired
-	//@Setter(onMethod_ = { @Autowired })
-	@Autowired
+	@Setter(onMethod_ = { @Autowired })
+	//@Autowired
 	private DataSource dataSource; 
 	
 	@Autowired
--- spring_project/Spring-Openproject/src/main/java/com/aia/op/member/controller/MemberLogoutController.java
+++ spring_project/Spring-Openproject/src/main/java/com/aia/op/member/controller/MemberLogoutController.java
@@ -4,15 +4,16 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MemberLogoutController {
-	
-	@RequestMapping("/member/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		System.out.println("로그아웃!!!");
-		return "redirect:/";
-	}
-
+   
+   @RequestMapping("/member/logout")
+   public String logout(HttpSession session, RedirectAttributes rda) {
+      session.invalidate();
+      rda.addAttribute("type", "delete");
+      rda.addAttribute("result", "ok");
+      return "redirect:/";
+   }
 }
